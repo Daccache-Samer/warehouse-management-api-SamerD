a7561b896a3e5e8d9f7f9640e0e2c9935bd9a5b8
Daccache-Samer <samerdacc@gmail.com>
2026-07-02 15:37:34 +0300

    refactor(session-02): Added Input Validation

--- warehouse-management-api/warehouse-management-api/Models/Product.cs
+++ warehouse-management-api/warehouse-management-api/Models/Product.cs
@@ -1,17 +1,25 @@
-﻿namespace warehouse_management_api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace warehouse_management_api.Models;
 
 //creating product domain model
 public class Product
 {
-    public string Id { get; set; } = string.Empty;// since .net6 string cannot be nullable so we can add "=string.empty" to give a default value to the string. we can also add the required field or give it a type string? to make it nullable. 
-    public string Name { get; set; } = string.Empty;
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "invalid Id")]
+    public required string Id { get; set; } = string.Empty;// since .net6 string cannot be nullable so we can add "=string.empty" to give a default value to the string. we can also add the required field or give it a type string? to make it nullable. 
+    [StringLength(50,MinimumLength = 5,ErrorMessage = "invalid Name")]
+    public required string Name { get; set; } = string.Empty;
+    [StringLength(10,MinimumLength = 2,ErrorMessage = "invalid SKU")]
     public string SKU { get; set; } =  string.Empty;
+    [StringLength(500,MinimumLength = 5,ErrorMessage = "invalid Description")]
     public string Description { get; set; }  = string.Empty;
-    public int Price{get;set;}
-    public int QuantityInStock { get; set; }
-    public string SupplierName{ get; set; } = string.Empty;
-    public DateTime ExpiryDate { get; set; }
-    public bool IsArchived { get; set; }
+    
+    public required int Price{get;set;}
+    public required int QuantityInStock { get; set; }
+    [StringLength(50,MinimumLength = 5,ErrorMessage = "invalid SupplierName")]
+    public required string SupplierName{ get; set; } = string.Empty;
+    public required DateTime ExpiryDate { get; set; }
+    public required bool IsArchived { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime LastUpdatedAt { get; set; }
 }
--- warehouse-management-api/warehouse-management-api/Models/ProductImage.cs
+++ warehouse-management-api/warehouse-management-api/Models/ProductImage.cs
@@ -1,8 +1,13 @@
-﻿namespace warehouse_management_api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace warehouse_management_api.Models;
 
 public class ProductImage
 {
+    [StringLength(50,MinimumLength = 5,ErrorMessage = "invalid ProductId")]
     private string ProductId { get; set; } = string.Empty;
+    [StringLength(500,MinimumLength = 1,ErrorMessage = "invalid Filename")]
     private string FileName { get; set; } = string.Empty;
+    [StringLength(500,MinimumLength = 5,ErrorMessage = "invalid Filepath")]
     private string FilePath { get; set; } = string.Empty;
 }
