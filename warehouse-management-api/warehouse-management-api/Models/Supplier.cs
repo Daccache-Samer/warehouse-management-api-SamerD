557139905d1678f362987d0c021024fc1bdb0813
Daccache-Samer <samerdacc@gmail.com>
2026-07-03 14:44:38 +0300

    feat(session-02): created supplier model.

---
+++ warehouse-management-api/warehouse-management-api/Models/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace warehouse_management_api.Models;
+
+public class Supplier
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required string Country { get; set; }
+    public required string ContactEmail { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required bool IsActive { get; set; }
+}
