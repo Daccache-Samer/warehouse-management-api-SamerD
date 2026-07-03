ac7caca9e49597061854cc1d63beef25228d3833
Daccache-Samer <samerdacc@gmail.com>
2026-07-03 10:58:16 +0300

    refactor(session-02): created directory "data" and moved FakeWarehouseStore.cs into it. Removed code bloat from Product.cs.

---
+++ warehouse-management-api/warehouse-management-api/Data/FakeWarehouseStore.cs
@@ -0,0 +1,159 @@
+﻿using warehouse_management_api.Models;
+
+namespace warehouse_management_api.Data;
+
+public static class FakeWarehouseStore
+{
+    public static List<Product> Products { set; get; } =
+    [
+        new Product
+        {
+            Id = "1ea627ad-cd70-4de2-9d59-c65a5442b9e2",
+            Name = "laptop",
+            SKU = "AA",
+            Description = "Laptop",
+            Price = 100,
+            QuantityInStock = 10,
+            SupplierName = "Lenovo",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "cfbddbc1-c0a0-4414-8c66-678923118cf0",
+            Name = "GTA6",
+            SKU = "BB",
+            Description = "GTA6",
+            Price = 80,
+            QuantityInStock = 100,
+            SupplierName = "Rockstar",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "98bc6c67-7e70-4f3d-91c1-b5e79afad9bb",
+            Name = "PS5",
+            SKU = "CC",
+            Description = "PS5",
+            Price = 400,
+            QuantityInStock = 20,
+            SupplierName = "Sony",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "fdb8659d-28c9-4190-98d3-d5549476ba7a",
+            Name = "RAM",
+            SKU = "DD",
+            Description = "RAM",
+            Price = 10000,
+            QuantityInStock = 100,
+            SupplierName = "Nvidia",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "a365845c-9c26-4cb3-aa67-4cdd59dbbe4a",
+            Name = "pen",
+            SKU = "EE",
+            Description = "pen",
+            Price = 10,
+            QuantityInStock = 1000,
+            SupplierName = "Bic",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "1192c0d3-e542-46a7-bdff-d7baa8adf6bd",
+            Name = "Gaming Chairs",
+            SKU = "FF",
+            Description = "Gaming Chairs",
+            Price = 200,
+            QuantityInStock = 100,
+            SupplierName = "Alienware",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "e31f293c-31ad-4421-85e0-5fd24cf1ee53",
+            Name = "Bottle of water",
+            SKU = "GG",
+            Description = "Bottle of water",
+            Price = 1,
+            QuantityInStock = 1000,
+            SupplierName = "Tannourine",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "5e2db995-1f14-4fb4-abae-49198637ccc1",
+            Name = "textbook",
+            SKU = "HH",
+            Description = "textbook",
+            Price = 10,
+            QuantityInStock = 1000,
+            SupplierName = "Bic",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "ff17297a-b575-44b4-b394-593cc31211d7",
+            Name = "Phones",
+            SKU = "II",
+            Description = "Phones",
+            Price = 400,
+            QuantityInStock = 500,
+            SupplierName = "Xiaomi",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        },
+
+        new Product
+        {
+            Id = "76c46cf4-e8ba-4906-8cbe-5e5abcd1bfb4",
+            Name = "sneakers",
+            SKU = "JJ",
+            Description = "sneakers",
+            Price = 300,
+            QuantityInStock = 1000,
+            SupplierName = "Nike",
+            ExpiryDate = DateTime.Now,
+            IsArchived = false,
+            CreatedAt = DateTime.Now,
+            LastUpdatedAt = DateTime.Now
+        }
+    ];
+}
--- warehouse-management-api/warehouse-management-api/Models/Product.cs
+++ warehouse-management-api/warehouse-management-api/Models/Product.cs
@@ -6,18 +6,17 @@ namespace warehouse_management_api.Models;
 public class Product
 {
     [StringLength(50, MinimumLength = 5, ErrorMessage = "invalid Id")]
-    public required string Id { get; set; } = string.Empty;// since .net6 string cannot be nullable so we can add "=string.empty" to give a default value to the string. we can also add the required field or give it a type string? to make it nullable. 
+    public required string Id { get; set; } // since .net6 string cannot be nullable so we can add "=string.empty" to give a default value to the string. we can also add the required field or give it a type string? to make it nullable. 
     [StringLength(50,MinimumLength = 5,ErrorMessage = "invalid Name")]
-    public required string Name { get; set; } = string.Empty;
+    public required string Name { get; set; }
     [StringLength(10,MinimumLength = 2,ErrorMessage = "invalid SKU")]
     public string SKU { get; set; } =  string.Empty;
     [StringLength(500,MinimumLength = 5,ErrorMessage = "invalid Description")]
     public string Description { get; set; }  = string.Empty;
-    
     public required int Price{get;set;}
     public required int QuantityInStock { get; set; }
     [StringLength(50,MinimumLength = 5,ErrorMessage = "invalid SupplierName")]
-    public required string SupplierName{ get; set; } = string.Empty;
+    public required string SupplierName{ get; set; } 
     public required DateTime ExpiryDate { get; set; }
     public required bool IsArchived { get; set; }
     public DateTime CreatedAt { get; set; }
