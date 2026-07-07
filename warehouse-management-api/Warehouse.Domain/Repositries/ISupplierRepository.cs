aa56ede7b5392cf5716405eaac2773c41d89d178
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 00:01:53 +0300

    feat(session-03): Created repositry interfaces

---
+++ warehouse-management-api/Warehouse.Domain/Repositries/ISupplierRepository.cs
@@ -0,0 +1,12 @@
+﻿using Warehouse.DomainWarehouse.Domain.Models;
+
+namespace Warehouse.DomainWarehouse.Domain.Repositries;
+
+public interface ISupplierRepository
+{
+    Task<IEnumerable<Supplier>> GetAllAsync();
+    Task<Supplier?> GetByIdAsync(string id);
+    Task AddAsync(Supplier supplier);
+    Task UpdateAsync(Supplier supplier);
+    Task<bool> ExistsAsync(string id);
+}
