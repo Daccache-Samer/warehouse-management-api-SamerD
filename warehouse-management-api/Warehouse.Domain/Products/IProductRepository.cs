2cf1b21bb178368c691d2f6e12610b52b881843b
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 00:01:53 +0300

    feat(session-03): Created repositry interfaces

---
+++ warehouse-management-api/Warehouse.Domain/Products/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Warehouse.DomainWarehouse.Domain.Products;
+
+public interface IProductRepository
+{
+    Task<Product?> GetByIdAsync(string id, CancellationToken ct = default);
+    Task<Product?> GetBySkuAsync(string sku, CancellationToken ct = default);
+    Task<IReadOnlyList<Product>> GetAllAsync(CancellationToken ct = default);
+    Task AddAsync(Product product, CancellationToken ct = default);
+    Task UpdateAsync(Product product, CancellationToken ct = default);
+}
