db19568d1c1b78ac75416726ba005766f5952e17
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 09:32:59 +0300

    feat(session-03): Created UpdateProductPriceCommand

---
+++ warehouse-management-api/Warehouse.Application/Products/Commands/UpdateProductPrice/UpdateProductPriceCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Warehouse.Application.Products.Commands.UpdateProductPrice;
+
+public abstract record UpdateProductPriceCommand(string ProductId, decimal Price) : IRequest<ProductDto>;
