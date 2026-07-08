1f6630f1ed76b4f243bfdb0ab78031c292bffc67
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 09:33:15 +0300

    feat(session-03): Created UpdateProductQuantityCommand

---
+++ warehouse-management-api/Warehouse.Application/Products/Commands/UpdateProductQuantity/UpdateProductQuantityCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Warehouse.Application.Products.Commands.UpdateProductQuantity;
+
+public abstract record UpdateProductQuantityCommand(string ProductId, int QuantityInStock) : IRequest<ProductDto>;
---
+++ warehouse-management-api/Warehouse.Application/Products/Commands/UpdateProductQuantity/UpdateProductQuantityHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Warehouse.Application.Exceptions;
+using Warehouse.DomainWarehouse.Domain.Products;
+
+namespace Warehouse.Application.Products.Commands.UpdateProductQuantity;
+
+public class UpdateProductQuantityHandler(IProductRepository productRepository)
+    : IRequestHandler<UpdateProductQuantityCommand, ProductDto>
+{
+    public async Task<ProductDto> Handle(UpdateProductQuantityCommand request, CancellationToken cancellationToken)
+    {
+        var product = await productRepository.GetByIdAsync(request.ProductId, cancellationToken)
+                      ?? throw new NotFoundException($"Product with id '{request.ProductId}' was not found.");
+
+        product.UpdateQuantity(request.QuantityInStock);
+
+        await productRepository.UpdateAsync(product, cancellationToken);
+
+        return ProductDto.FromDomain(product);
+    }
+}
