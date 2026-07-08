be253250b3278f1fe657d484e2ea4908327e9dde
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 09:32:11 +0300

    feat(session-03): Created ListSuppliersQuery

---
+++ warehouse-management-api/Warehouse.Application/Suppliers/Queries/ListSuppliers/ListSuppliersHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Warehouse.DomainWarehouse.Domain.Suppliers;
+
+namespace Warehouse.Application.Suppliers.Queries.ListSuppliers;
+
+public class ListSuppliersHandler(ISupplierRepository supplierRepository)
+    : IRequestHandler<ListSuppliersQuery, IReadOnlyList<SupplierDto>>
+{
+    public async Task<IReadOnlyList<SupplierDto>> Handle(ListSuppliersQuery request, CancellationToken cancellationToken)
+    {
+        var suppliers = await supplierRepository.GetAllAsync(cancellationToken);
+        return suppliers.Select(SupplierDto.FromDomain).ToList();
+    }
+}
---
+++ warehouse-management-api/Warehouse.Application/Suppliers/Queries/ListSuppliers/ListSuppliersQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Warehouse.Application.Suppliers.Queries.ListSuppliers;
+
+public abstract record ListSuppliersQuery : IRequest<IReadOnlyList<SupplierDto>>;
