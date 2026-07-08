9ba59cc6acc7dcc824652299c9c19f09683bf492
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 09:37:23 +0300

    refactor(session-03): Deleted files that are not needed anymore

--- warehouse-management-api/Warehouse.Application/Contracts/CreateSupplierRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Warehouse.Application;
-
-public class CreateSupplierRequest
-{
-    public string Name { get; set; } = string.Empty;
-    public string Country { get; set; } = string.Empty;
-    public string ContactEmail { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
-}
