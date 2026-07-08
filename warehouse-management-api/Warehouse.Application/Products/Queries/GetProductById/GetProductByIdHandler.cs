14e177c00139dcb5083533e668d2f8963af0c82c
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 09:31:17 +0300

    feat(session-03): Created GetProductByIdQuery

---
+++ warehouse-management-api/Warehouse.Application/Products/Queries/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Warehouse.DomainWarehouse.Domain.Products;
+
+namespace Warehouse.Application.Products.Queries.GetProductById;
+
+public class GetProductByIdHandler(IProductRepository productRepository)
+    : IRequestHandler<GetProductByIdQuery, ProductDto?>
+{
+    public async Task<ProductDto?> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        var product = await productRepository.GetByIdAsync(request.ProductId, cancellationToken);
+        return product is null ? null : ProductDto.FromDomain(product);
+    }
+}
---
+++ warehouse-management-api/Warehouse.Application/Products/Queries/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Warehouse.Application.Products.Queries.GetProductById;
+
+public abstract record GetProductByIdQuery(string ProductId) : IRequest<ProductDto?>;
