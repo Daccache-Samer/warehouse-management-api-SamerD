2ec7eb3feb0798d174248ce9673c50d092d66d7d
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 09:31:59 +0300

    feat(session-03): Created ListProductsQuery

---
+++ warehouse-management-api/Warehouse.Application/Products/Queries/ListProducts/ListProductsQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Warehouse.Application.Products.Queries.ListProducts;
+
+public abstract record ListProductsQuery(bool OnlyAvailable = false) : IRequest<IReadOnlyList<ProductDto>>;
