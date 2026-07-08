b17af097380a06d5d0c8d52966c565dec9a27841
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 09:29:18 +0300

    feat(session-03): created ArchiveProductCommand

---
+++ warehouse-management-api/Warehouse.Application/Products/Commands/ArchiveProduct/ArchiveProductCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Warehouse.Application.Products.Commands.ArchiveProduct;
+
+public abstract record ArchiveProductCommand(string ProductId) : IRequest;
---
+++ warehouse-management-api/Warehouse.Application/Products/Commands/ArchiveProduct/ArchiveProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Warehouse.Application.Exceptions;
+using Warehouse.DomainWarehouse.Domain.Products;
+
+namespace Warehouse.Application.Products.Commands.ArchiveProduct;
+
+public class ArchiveProductHandler(IProductRepository productRepository) : IRequestHandler<ArchiveProductCommand>
+{
+    public async Task Handle(ArchiveProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await productRepository.GetByIdAsync(request.ProductId, cancellationToken)
+                      ?? throw new NotFoundException($"Product with id '{request.ProductId}' was not found.");
+
+        product.Archive();
+
+        await productRepository.UpdateAsync(product, cancellationToken);
+    }
+}
