71974ebf4979136413d78706f581ddac296bf9ac
Daccache-Samer <samerdacc@gmail.com>
2026-07-08 09:30:49 +0300

    feat(session-03): Created CreateSupplierCommand

---
+++ warehouse-management-api/Warehouse.Application/Suppliers/Commands/CreateSupplier/CreateSupplierCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Warehouse.Application.Suppliers.Commands.CreateSupplier;
+
+public abstract record CreateSupplierCommand(
+    string Name,
+    string Country,
+    string ContactEmail,
+    string PhoneNumber) : IRequest<SupplierDto>;
---
+++ warehouse-management-api/Warehouse.Application/Suppliers/Commands/CreateSupplier/CreateSupplierHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Warehouse.DomainWarehouse.Domain.Suppliers;
+
+namespace Warehouse.Application.Suppliers.Commands.CreateSupplier;
+
+public class CreateSupplierHandler(ISupplierRepository supplierRepository)
+    : IRequestHandler<CreateSupplierCommand, SupplierDto>
+{
+    public async Task<SupplierDto> Handle(CreateSupplierCommand request, CancellationToken cancellationToken)
+    {
+        var supplier = Supplier.Create(request.Name, request.Country, request.ContactEmail, request.PhoneNumber);
+
+        await supplierRepository.AddAsync(supplier, cancellationToken);
+
+        return SupplierDto.FromDomain(supplier);
+    }
+}
