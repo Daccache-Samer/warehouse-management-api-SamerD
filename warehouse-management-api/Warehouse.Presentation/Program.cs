02f8c9e9f501649a79d0c886a66db2ba070c807d
Daccache-Samer <samerdacc@gmail.com>
2026-07-11 13:57:24 +0300

    feat(session-04): Added dbcontext injection

--- warehouse-management-api/Warehouse.Presentation/Program.cs
+++ warehouse-management-api/Warehouse.Presentation/Program.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 using Warehouse.Application.Products.Commands.CreateProduct;
 using Warehouse.DomainWarehouse.Domain.Products;
@@ -6,11 +7,14 @@ using Warehouse.DomainWarehouse.Domain.Suppliers;
 using Warehouse.Infrastructure.Persistence.InMemory;
 using Warehouse.Infrastructure.Storage;
 using warehouse_management_api.Middleware;
+using Warehouse.Infrastructure.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var configuration = builder.Configuration;
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<WarehouseDbFirstContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("Default Connection")));
 builder.Services.AddSwaggerGen(options =>
 {
     options.MapType<IFormFile>(() => new OpenApiSchema
