1edeee51e9068039253f7ba37e91126642ec2983
Daccache-Samer <samerdacc@gmail.com>
2026-07-16 13:00:20 +0300

    fix(session-05): The examples generated by swagger to test api endpoints were triggering validation errors so I added XML doc comment <example> tags to be able to test them successfully.

--- warehouse-management-api/Warehouse.Presentation/Contracts/UpdateProductPriceRequest.cs
+++ warehouse-management-api/Warehouse.Presentation/Contracts/UpdateProductPriceRequest.cs
@@ -4,6 +4,7 @@ namespace warehouse_management_api.Contracts;
 
 public class UpdateProductPriceRequest
 {
+    /// <example>50</example>
     [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
     public decimal Price { get; set; }
 }
--- warehouse-management-api/Warehouse.Presentation/Program.cs
+++ warehouse-management-api/Warehouse.Presentation/Program.cs
@@ -33,6 +33,9 @@ builder.Services.AddSwaggerGen(options =>
         Type = JsonSchemaType.String,
         Format = "binary"
     });
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
 });
 
 builder.Services.AddMediatR(cfg =>
