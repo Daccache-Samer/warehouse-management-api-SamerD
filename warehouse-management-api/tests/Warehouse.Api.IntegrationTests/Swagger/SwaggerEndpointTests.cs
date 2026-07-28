83d62d589701b99451502f0fde730e9a8f4acb4e
Daccache-Samer <samerdacc@gmail.com>
2026-07-28 23:37:53 +0300

    feat(session-09): add swagger integration test.

---
+++ warehouse-management-api/tests/Warehouse.Api.IntegrationTests/Swagger/SwaggerEndpointTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using FluentAssertions;
+
+namespace Warehouse.Api.IntegrationTests.Swagger;
+
+public class SwaggerEndpointTests(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+
+    [Fact]
+    public async Task GET_SwaggerJson_ReturnsSuccessAndValidDocument()
+    {
+        // Act
+        var response = await _client.GetAsync("/swagger/v1/swagger.json");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Content.Headers.ContentType?.MediaType.Should().Be("application/json");
+
+        var body = await response.Content.ReadAsStringAsync();
+        body.Should().Contain("\"openapi\"");
+    }
+}
