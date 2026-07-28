f4f2944bbf2b672951c0e4fd53ac70f02d5053bf
Daccache-Samer <samerdacc@gmail.com>
2026-07-28 23:37:39 +0300

    feat(session-09): add product image integration test.

---
+++ warehouse-management-api/tests/Warehouse.Api.IntegrationTests/TestUtilities/Helpers/MultipartFormHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+namespace Warehouse.Api.IntegrationTests.TestUtilities.Helpers;
+
+public static class MultipartFormHelper
+{
+    public static MultipartFormDataContent CreateFileContent(
+        string fieldName, string fileName, string contentType, byte[] fileBytes)
+    {
+        var content = new MultipartFormDataContent();
+        var fileContent = new ByteArrayContent(fileBytes);
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+        content.Add(fileContent, fieldName, fileName);
+        return content;
+    }
+}
